4baa15d920ec3616e4cac946f4bf09ad9ca14800
Brandon Schuehlein <brandon@schuehlein.eu>
2025-05-28 11:05:07 +0200

    add java formatter

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -26,6 +26,7 @@ return {
 		end,
 		formatters_by_ft = {
 			lua = { "stylua" },
+			java = { "google-java-format" },
 			-- Conform can also run multiple formatters sequentially
 			-- python = { "isort", "black" },
 			--
